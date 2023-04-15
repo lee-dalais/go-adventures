1a8e6b444bdc1213962607fe07712511438a9de7
Lee Dalais <52302981+lee-dalais@users.noreply.github.com>
2023-04-15 16:32:49 -0700

    feat: first pass

---
+++ src/ex-1-saying-hello/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+import "math/rand"
+
+func main() {
+	var firstName string
+	greetings := []string{
+		"Hello",
+		"Howdy",
+		"How's it going",
+		"Hi",
+	}
+
+	fmt.Println("What is you name?")
+	_, err := fmt.Scanln(&firstName)
+	if err != nil {
+		return
+	}
+
+	fmt.Printf("%s, %s, nice to meet you! ", greetings[rand.Intn(len(greetings))], firstName)
+}
